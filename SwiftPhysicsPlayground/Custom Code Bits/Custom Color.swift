aebbc5ee13eb692491bf2067e8b06a6d5715caa6
SuperbiaR <altierstuff@gmail.com>
2021-12-01 14:49:55 -0600

    Finished All but detailing

--- SwiftPhysicsPlayground/Custom Code Bits/Custom Color.swift
+++ SwiftPhysicsPlayground/Custom Code Bits/Custom Color.swift
@@ -1,6 +1,6 @@
 public struct Color {
     public var r, g, b, a: UInt8
-    
+
     public init(r: UInt8, g: UInt8, b: UInt8, a: UInt8 = 255) {
         self.r = r
         self.g = g
@@ -11,5 +11,7 @@ public struct Color {
 
 // Create Custom Colors Here
 public extension Color {
-    
+    static let clear = Color(r: 0, g: 0, b: 0, a: 0)
+    static let backgroundGray = Color(r: 38, g: 38, b: 38, a: 1)
+    static let debugRed = Color(r: 100, g: 0, b: 0, a: 1)
 }
--- SwiftPhysicsPlayground/GameScene.swift
+++ SwiftPhysicsPlayground/GameScene.swift
@@ -2,10 +2,23 @@ import SwiftUI
 import SpriteKit
 import UIKit
 
+/*
+ ZPosition Tiers
+ Background = 0
+ Buttons = 1
+ UIModifiers = 2
+ Gameplay Objects = 3
+ Player = 4
+ Tints = 5
+ Debug = 7
+ Puff = 1000
+ */
+
 class pinnedOBJ: SKSpriteNode { }
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     // Object Defining Jesus theres a lot of this
+    let squareReference = SKSpriteNode(imageNamed: "Square?")
     let player = SKSpriteNode(imageNamed: "playerOBJ")
     let buttonL = SKSpriteNode(imageNamed: "buttonOBJ")
     let buttonR = SKSpriteNode(imageNamed: "buttonOBJ")
@@ -25,12 +38,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     let textS = SKLabelNode(text: "STOP")
     let textP = SKLabelNode(text: "PUFF TOGGLE")
     let textBounce = SKLabelNode(text: "BOUNCE AMOUNT: 0.0")
+    let platformBottom = SKSpriteNode(color: UIColor.red, size: CGSize(width: 100, height: 20))
+    let platformLedgeL = SKSpriteNode(color: UIColor.red, size: CGSize(width: 15, height: 40))
+    let platformLedgeR = SKSpriteNode(color: UIColor.red, size: CGSize(width: 15, height: 40))
+    let platformBoundarary = SKSpriteNode(color: UIColor.clear, size: CGSize(width: 150, height: 200))
+    let imminentDoom = SKSpriteNode(color: UIColor.systemPink, size: CGSize(width: 9000, height: 9000))
+    let textDeath = SKLabelNode(text: "THERE IS NO ESCAPE THIS IS THE END FOR YOU.")
     let logicNode = SKNode()
     
     
     // SKActions
     let audioDead = SKAction.playSoundFileNamed("Scout_paincrticialdeath02", waitForCompletion: false)
-    
     
     // Booleans
     public var movingLeft: Bool = false {
@@ -60,69 +78,112 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             
         }
     }
+    public var loomingThreatLevel: CGFloat = 0.00 {
+        didSet {
+            imminentDoom.alpha = loomingThreatLevel
+        }
+    }
     
     // Integers
     public var jumpForce: Int = 3000
     public var moveSpeed: Int = 300
     public var puffAutoMove: Int = 20
-    public var loomingThreatLevel: Int = 0
     public var bumperAmount: Int = 10 {
         didSet {
             resetAll()
+            
         }
     }
     
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
+        let playerRadius = player.frame.width / 2.0
         
         // Backdrop
         backdrop.position = CGPoint(x: 425, y: 100)
+        backdrop.zPosition = 0.1
         backdropRim.position = CGPoint(x: 425, y: 200)
-        backdropRim.zPosition = 100
+        backdropRim.zPosition = 2
         addChild(backdropRim)
         addChild(backdrop)
         
+        // Immeniant Doom Counter
+        imminentDoom.alpha = loomingThreatLevel
+        addChild(imminentDoom)
         
+        // Make A Single Platform SMH why the heck does this take this much code for this one thing.
+        platformBottom.physicsBody = SKPhysicsBody(texture: squareReference.texture!, size: platformBottom.size)
+        platformBottom.physicsBody?.allowsRotation = false
+        platformBottom.physicsBody?.pinned = true
+        platformBottom.position = CGPoint(x: 500, y: 500)
+        platformBottom.zPosition = 3
+        platformLedgeL.physicsBody = SKPhysicsBody(texture: squareReference.texture!, size: platformLedgeL.size)
+        platformLedgeL.position = CGPoint(x: platformBottom.position.x - 45, y: platformBottom.position.y + 10)
+        platformLedgeL.physicsBody?.allowsRotation = false
+        platformLedgeL.physicsBody?.pinned = true
+        platformLedgeL.zPosition = 3
+        platformLedgeR.physicsBody = SKPhysicsBody(texture: squareReference.texture!, size: platformLedgeR.size)
+        platformLedgeR.position = CGPoint(x: platformBottom.position.x + 45, y: platformBottom.position.y + 10)
+        platformLedgeR.physicsBody?.allowsRotation = false
+        platformLedgeR.physicsBody?.pinned = true
+        platformBoundarary.position = platformBoundarary.position
+        platformLedgeR.zPosition = 3
+        platformBoundarary.physicsBody = SKPhysicsBody(texture: squareReference.texture!, size: platformBoundarary.size)
+        platformBoundarary.position = platformBottom.position
+        platformBoundarary.physicsBody?.allowsRotation = false
+        platformBoundarary.physicsBody?.pinned = true
+        platformBoundarary.zPosition = 7
+        addChild(platformBottom)
+        addChild(platformLedgeL)
+        addChild(platformLedgeR)
+        addChild(platformBoundarary)
+        
+        // Setting up the spinning bumpers
         for _ in 1...bumperAmount {
             let bumper = pinnedOBJ(imageNamed: "playerOBJ")
-            bumper.physicsBody = SKPhysicsBody(texture: pinned.texture!, size: pinned.size)
-            bumper.position = CGPoint(x: 320, y: 600)
+            bumper.physicsBody = SKPhysicsBody(circleOfRadius: playerRadius + 50)
+            bumper.position = CGPoint(x: 400, y: 800)
             bumper.zRotation = CGFloat.pi / 2
             bumper.physicsBody?.pinned = false
             bumper.physicsBody?.restitution = 1
+            bumper.zPosition = 3
             addChild(bumper)
             bumper.physicsBody?.applyForce(CGVector(dx: Int.random(in: -10000...10000), dy: Int.random(in: -1000...1000)))
-            
+
             Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { _ in
+                try? self.platformBoundarary.removeFromParent()
+                bumper.physicsBody = SKPhysicsBody(circleOfRadius: playerRadius)
                 bumper.physicsBody?.pinned.toggle()
                 
             }
         }
         
+        // Left Slope
         slopeL.position = CGPoint(x: 40, y: 245)
         slopeL.physicsBody = SKPhysicsBody(texture: slopeL.texture!, size: slopeL.size)
         slopeL.physicsBody!.pinned = true
         slopeL.physicsBody!.allowsRotation = false
+        slopeL.zPosition = 1
         addChild(slopeL)
         
+        // Right Slope
         slopeR.position = CGPoint(x: 790, y: 245)
         slopeR.physicsBody = SKPhysicsBody(texture: slopeR.texture!, size: slopeR.size)
         slopeR.physicsBody!.pinned = true
         slopeR.physicsBody!.allowsRotation = false
+        slopeR.zPosition = 1
         addChild(slopeR)
         
-        let playerRadius = player.frame.width / 2.0
+        // Spawning in the Player Object
         player.physicsBody = SKPhysicsBody(circleOfRadius: playerRadius)
         player.physicsBody?.restitution = bouncyLevel
         player.physicsBody?.allowsRotation = true
         player.position = CGPoint(x: 200, y: 500)
         player.physicsBody?.pinned = false
         player.physicsBody?.friction = 0
-        player.zPosition = 100
+        player.zPosition = 4
         player.name = "Jimmy"
-        
         addChild(player)
-        
         
         // Puff????
         // yes
@@ -137,13 +198,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         puff.name = "puff"
         addChild(puff)
         
-        
         // Button Left
         buttonL.name = "Left"
         buttonL.size = buttonSize
         buttonL.position = CGPoint(x: 100, y: buttonHeightPOS)
+        buttonL.zPosition = 1
         textL.position = CGPoint(x: buttonL.position.x, y: buttonHeightPOS - 100)
         textL.fontColor = .black
+        textL.zPosition = 1
         self.addChild(buttonL)
         self.addChild(textL)
         
@@ -151,8 +213,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         buttonR.name = "Right"
         buttonR.size = buttonSize
         buttonR.position = CGPoint(x: 740, y: buttonHeightPOS)
+        buttonR.zPosition = 1
         textR.position = CGPoint(x: buttonR.position.x, y: buttonHeightPOS - 100)
         textR.fontColor = .black
+        textR.zPosition = 1
         self.addChild(buttonR)
         self.addChild(textR)
         
@@ -160,8 +224,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         buttonJUMP.name = "Jump"
         buttonJUMP.size = buttonSize
         buttonJUMP.position = CGPoint(x: (buttonR.position.x + buttonL.position.x)/2 - 70, y: buttonHeightPOS)
+        buttonJUMP.zPosition = 1
         textJ.position = CGPoint(x: buttonJUMP.position.x, y: buttonHeightPOS - 100)
         textJ.fontColor = .black
+        textJ.zPosition = 1
         self.addChild(buttonJUMP)
         self.addChild(textJ)
         
@@ -169,8 +235,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         buttonSTOP.name = "Stop"
         buttonSTOP.size = buttonSize
         buttonSTOP.position = CGPoint(x: (buttonR.position.x + buttonL.position.x)/2 + 70, y: buttonHeightPOS)
+        buttonSTOP.zPosition = 1
         textS.position = CGPoint(x: buttonSTOP.position.x, y: buttonHeightPOS - 100)
         textS.fontColor = .black
+        textS.zPosition = 1
         self.addChild(buttonSTOP)
         self.addChild(textS)
         
@@ -178,8 +246,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         buttonBounce.name = "Bounce"
         buttonBounce.size = CGSize(width: 50, height: 50)
         buttonBounce.position = CGPoint(x: (buttonR.position.x + buttonL.position.x)/2 + 150, y: 1150)
+        buttonBounce.zPosition = 1
         textBounce.position = CGPoint(x: buttonBounce.position.x, y: buttonBounce.position.y - 50)
         textBounce.fontColor = .white
+        textBounce.zPosition = 1
         self.addChild(buttonBounce)
         self.addChild(textBounce)
         
@@ -187,22 +257,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         buttonPuffToggle.name = "Puff Toggle"
         buttonPuffToggle.size = CGSize(width: 50, height: 50)
         buttonPuffToggle.position = CGPoint(x: (buttonR.position.x + buttonL.position.x)/2 - 150, y: 1150)
+        buttonPuffToggle.zPosition = 10
         textP.position = CGPoint(x: buttonPuffToggle.position.x, y: buttonPuffToggle.position.y - 50)
         textP.fontColor = .systemPink
+        textP.zPosition = 1
         self.addChild(buttonPuffToggle)
         self.addChild(textP)
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame.inset(by: UIEdgeInsets(top: 200, left: 0, bottom: 100, right: 0)))
         
-        
         // Movement Cause I Can
         Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [self] timer in
             print("Player Velocity:")
             print(player.physicsBody?.velocity as Any)
-            
-            
             print("Puff Position")
-            
             
             if puff.parent == nil {
                 print("Puff Is Not Here")
@@ -212,14 +280,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 
             }
             
-            
             if (movingLeft == true) {
                 if (Int((player.physicsBody?.velocity.dx)!) > -moveSpeed) {
                     player.physicsBody?.applyForce(CGVector(dx: -moveSpeed - Int((player.physicsBody?.velocity.dx)!), dy: 0))
                     
                 }
-                
-                
             }
             
             if (movingRight == true) {
@@ -227,7 +292,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                     player.physicsBody?.applyForce(CGVector(dx: moveSpeed - Int((player.physicsBody?.velocity.dx)!), dy: 0))
                     
                 }
-                
             }
             
             if (player.position.y < -300) {
@@ -236,11 +300,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             }
             
             if (brake == true) {
-                if (Double((player.physicsBody?.velocity.dx)!) < 1) {
-                    player.physicsBody?.applyForce(CGVector(dx: 100, dy: 0))
+                if (Double((player.physicsBody?.velocity.dx)!) > 0) {
+                    player.physicsBody?.applyForce(CGVector(dx: -(player.physicsBody?.velocity.dx)!, dy: 0))
                     
-                } else if (Double((player.physicsBody?.velocity.dx)!) > -1) {
-                    player.physicsBody?.applyForce(CGVector(dx: -100, dy: 0))
+                } else if (Double((player.physicsBody?.velocity.dx)!) > 0) {
+                    player.physicsBody?.applyForce(CGVector(dx: -(player.physicsBody?.velocity.dx)!, dy: 0))
+                    
+                }
+                
+                if (Double((player.physicsBody?.velocity.dx)!) < 1 && Double((player.physicsBody?.velocity.dx)!) > -1) {
+                    player.physicsBody?.velocity = CGVector(dx: 0, dy: (player.physicsBody?.velocity.dy)!)
+                    brake = false
                     
                 }
             }
@@ -249,6 +319,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 // Dont crash
                 if (Int(puff.position.y) < 700) {
                     puff.physicsBody?.velocity = CGVector(dx: (puff.physicsBody?.velocity.dx)!, dy: -((puff.physicsBody?.velocity.dy)!))
+                    
                 }
                 if (/* Greater then -20 */ (Int(puff.physicsBody!.velocity.dx) > -puffAutoMove) && (Int(puff.physicsBody!.velocity.dy) > -puffAutoMove) && /* Less then 20 */ (Int(puff.physicsBody!.velocity.dx) < puffAutoMove) && (Int(puff.physicsBody!.velocity.dy) < puffAutoMove)) {
                     
@@ -322,7 +393,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 if (puff.parent != nil) {
                     puff.removeFromParent()
                     playSound(sound: audioDead)
+                    loomingThreatLevel += 0.02
+                    if loomingThreatLevel >= 1 {
+                        imminentDoom.zPosition += 1
+                        
+                    }
                     
+                    if loomingThreatLevel == 7 {
+                        textDeath.position = CGPoint(x: (buttonR.position.x + buttonL.position.x)/2, y: 600)
+                        textDeath.fontColor = .black
+                        textDeath.zPosition = 11
+                        self.addChild(textDeath)
+                        
+                    }
                 } else {
                     self.addChild(puff)
                     
